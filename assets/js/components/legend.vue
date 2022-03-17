7a8ecd61a73d5e7562ed71b5bacdfb58c315c3a6
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-17 16:56:45 +0100

    Vue - props: passing info into a child component

---
+++ assets/js/components/legend.vue
@@ -0,0 +1,12 @@
+<template>
+    <span class="p-3">
+        {{ title }}
+    </span>
+</template>
+
+<script>
+export default {
+    name: 'Legend',
+    props: ['title'],
+};
+</script>
--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -42,9 +42,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <span class="p-3">
-                        {{ legend }}
-                    </span>
+                    <LegendComponent title="TODO PUT LEGEND HERE" />
                 </div>
             </div>
         </div>
@@ -52,8 +50,12 @@
 </template>
 
 <script>
+import LegendComponent from '../components/legend';
 export default {
     name: 'Products',
+    components: {
+        LegendComponent,
+    },
     data() {
         return {
             legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
