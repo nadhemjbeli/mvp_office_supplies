7b22d99da60bff174232211684d3bc9efe075cbd
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-17 05:06:46 +0100

    Vue - vue instance & dynamic data

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,6 +1,17 @@
 import Vue from 'vue';
 
+const template = '<h1>Hello {{ firstName }}! Is this cooler?</h1>';
+
 const app = new Vue({
     el: '#app',
-    template: '<h1>Hello vue! </h1>',
+    data() {
+        return {
+            firstName: 'Ryan',
+        };
+    },
+    render(h) {
+        return Vue.compile(template).render.call(this, h);
+    },
 });
+
+window.app = app;
