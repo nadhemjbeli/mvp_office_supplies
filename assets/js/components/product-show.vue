60e8199116c3167d11afb34f8bb87eead950a7a7
nadhemjbeli <nadhem98jbeli@gmail.com>
2022-03-19 17:41:19 +0100

    vue - Page 2: The Dynamic Component

---
+++ assets/js/components/product-show.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        I'd ❤️ to see Product {{ productId }} here!
+    </div>
+</template>
+<script>
+export default {
+    name: 'ProductShow',
+    props: {
+        productId: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
